7ec0d7ced5b9355d680afaa203492480a4e698ed
Ali Karbassi <ali@karbassi.com>
2021-11-19 19:42:19 -0600

    const

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,12 @@
 // To see things inside the browser's console:
 // console.log('Hey there')
 
+// var - don't use
 var my_name = 'Ali Karbassi'
 
 console.log(my_name)
 
+// let - mutable
 let fav_planet = 'Earth'
 
 console.log(fav_planet)
@@ -12,3 +14,11 @@ console.log(fav_planet)
 fav_planet = 'Saturn'
 
 console.log(fav_planet)
+
+// const - immutable
+const fav_color = 'black'
+
+console.log(fav_color)
+
+// This won't work!
+// fav_color = 'yellow'
