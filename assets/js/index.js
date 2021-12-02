cc96775e5d36cbcb1287c6a32a52ba47e0adb7fe
Ali Karbassi <ali@karbassi.com>
2021-12-01 19:00:08 -0600

    cleanup

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,4 @@
 function doAddition(firstElementID, secondElementID, sumElementID) {
-  console.log('doAddition() called')
-  console.log({ firstElementID })
-  console.log({ secondElementID })
-  console.log({ sumElementID })
-
   // get 'first' HTML element
   const $first = document.querySelector(firstElementID)
 
@@ -17,13 +12,10 @@ function doAddition(firstElementID, secondElementID, sumElementID) {
   // add 'first-value' and 'second-value' together
   const sum = firstValue + secondValue
 
-  // console.log(sum)
-
   // display the sum in 'sum' HTML element
 
   // get 'sum' HTML element
   const $sum = document.querySelector(sumElementID)
-  // console.log($sum)
 
   $sum.value = sum
 }
